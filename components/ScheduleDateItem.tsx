d8aebdd115c5d2ac9affed101ab1a5b44e9e1201
vikrant22 <vikrant@usefreebird.com>
2024-09-25 20:51:48 +0530

    clear component folder

---
+++ components/ScheduleDateItem.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ScheduleDateItem = () => {
+  return (
+    <View>
+      <Text>ScheduleDateItem</Text>
+    </View>
+  );
+};
+
+export default ScheduleDateItem;
+
+const styles = StyleSheet.create({});
